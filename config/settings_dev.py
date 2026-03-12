98e8dacea99f08b7fd4ca0992951f16d352637d7
forevercj18 <forevercj18@users.noreply.github.com>
2026-03-12 17:09:47 +0800

    chore: allow trycloudflare host in dev settings

--- config/settings_dev.py
+++ config/settings_dev.py
@@ -6,4 +6,4 @@ from .settings_common import *  # noqa: F401,F403
 
 
 DEBUG = env_bool("DEBUG", True)
-ALLOWED_HOSTS = env_list("ALLOWED_HOSTS", "127.0.0.1,localhost")
+ALLOWED_HOSTS = env_list("ALLOWED_HOSTS", "127.0.0.1,localhost,.trycloudflare.com")
